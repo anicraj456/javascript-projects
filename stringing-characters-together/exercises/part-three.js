546cbf6f3c19ded372ed66a39a0657c421b381af
anicraj456 <anicraj123@gmail.com>
2023-09-16 12:27:48 -0500

    string completed

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -1,17 +1,40 @@
+
 //Part Three section one
 
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+let firstLetter = language.slice(0,1);
+let secondLetter = language.slice(4,5);
+console.log ("Using Slice Method : " + firstLetter + secondLetter);
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+ 
+let firstIni = language[0];
+let secondIni = language[4];
+console.log("Using Method Chaning : " + firstIni + secondIni);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+let firstAlpha = language[0];
+let secondAlpha = language[4];
+let inAlpha = ( firstAlpha + secondAlpha );
+console.log( "Using Bracket notation & Template Literal: " + `The abbreviation for '${language}' is '${inAlpha}'.` );
+
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+let ans = "  The abbreviation for 'JavaScript' is 'JS'.  "
+let trimmed = ans.trim().toLowerCase().replace('t','T').replace('j','J').replace('s','S').replace('js','JS');
+console.log("Using More Than 3 Methods : " + trimmed);
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let changeCase = notTitleCase.replace('t','T') .replace('c','C');
+console.log("Using Replace Method:" + changeCase);
+
+
+
--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -4,29 +4,53 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
 
+console.log("Current State:" + dna);
+
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
 
-console.log(/* Your code here. */);
+console.log("Using Trim Method: " + dna.trim()); /* Your code here. */
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
-console.log();
+console.log("Using Uppercase Method: " + dna.toUpperCase());
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
 
-console.log(dna);
+dna = dna.toUpperCase();
+console.log("New DNA: " + dna);
 
 //Part Two Section Two
 
 let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
+ 
+dnaTwo = dnaTwo.replace("GCT", "AGG");
+console.log("Using Replace Method: " + dnaTwo);
 
 //2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
 
+let dnaTwoIndex = dnaTwo.indexOf("CAT");
+console.log("CAT gene at DnaTwo index :" + dnaTwoIndex);
+
+let dnaTwoLength = dnaTwo.length;
+
+if (dnaTwoIndex < dnaTwoLength){
+    console.log("CAT gene found");
+} else{
+    console.log("CAT gene not found");
+}
+
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
+
+console.log("Sliced : " + dna.slice(17,20));
 
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 
+let dnaLength = dnaTwo.length;
+console.log("Using Template Literal :" + `The DNA strand is ${dnaLength} characters long.`);
+
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+ 
+console.log("Using Methods & Template Literal: " + `${dna.slice(5,8).toLowerCase()}o ${dna.slice(41,44).toLowerCase()}`); 
