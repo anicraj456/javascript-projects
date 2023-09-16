1b2fd8cda271f94d3d030a4ff2457f45ec55d2a9
anicraj456 <anicraj123@gmail.com>
2023-09-16 18:16:18 -0500

    loops exe

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
-
+let family = ["Siraj" ,"Anitha" ,"Apsara" ,"Arshad"];
 // write a for loop that prints each name on a different line
+
+for (i=0;i<(family.length);i++){
+    console.log(family[i]);
+}
