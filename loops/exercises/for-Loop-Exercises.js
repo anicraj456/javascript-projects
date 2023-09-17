bf03fe5091e4347dc385fbc6af789c94692a5727
anicraj456 <anicraj123@gmail.com>
2023-09-17 13:29:58 -0500

    Update for-Loop-Exercises.js

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -7,23 +7,21 @@
     //a. Print the numbers 0 - 20, one number per line.
 
     for (let i=0;i<21;i++){
-      console.log("Num Per Line: " + i);
+      //console.log("Num Per Line: " + i);
       }
 
     //b. Print only the ODD values from 3 - 29, one number per line.
 
     for(let j=2;j<30;j++){
         j=j+1;
-        console.log("Odd Value: " + j);
+        //console.log("Odd Value: " + j);
     }
     
     //c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-
-    for (let k=11;k<14;k++){
-     k=k+1
-      console.log(k);
+    let a=-14;
+    for (let k=12;k<=20;k-2){
+      console.log("the value of K" + k);
     }
-
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
@@ -33,7 +31,7 @@ Construct ``for`` loops to accomplish the following tasks:
   b. Print each character of the string - in reverse order - to a new line. */ 
  
   //Initializing variable
-
+/*
  let str = "LaunchCode";
  let arr = [1, 5, 'LC101', 'blue', 42];
  let reverse = "";
@@ -43,19 +41,19 @@ Construct ``for`` loops to accomplish the following tasks:
  for (let j= 0;j<arr.length;j++){
    console.log("Element of Array : " + arr[j]);
  }
-
+*/
  // b. Print each character of the string - in reverse order - to a new line. */ 
 
- for (let i = 0; i < str.length; i++) {
+ /*for (let i = 0; i < str.length; i++) {
   reverse = str[i] + reverse;
 }
-console.log("Reversed String: " + reverse);
+console.log("Reversed String: " + reverse); */
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
 
-  let arr1= [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  /*let arr1= [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
   let even=[];
   let odd=[];
   
@@ -65,3 +63,4 @@ console.log("Reversed String: " + reverse);
        console.log(even);
      } 
     }
+*/
