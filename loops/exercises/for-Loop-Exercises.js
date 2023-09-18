235e36cd6eb35ff0d5e28aa4d95ed71b35049d33
anicraj456 <anicraj123@gmail.com>
2023-09-18 16:02:17 -0500

    still working on it

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -7,20 +7,20 @@
     //a. Print the numbers 0 - 20, one number per line.
 
     for (let i=0;i<21;i++){
-      //console.log("Num Per Line: " + i);
+      console.log("Num Per Line: " + i);
       }
 
     //b. Print only the ODD values from 3 - 29, one number per line.
 
     for(let j=2;j<30;j++){
         j=j+1;
-        //console.log("Odd Numbers: " + j);
+        console.log("Odd Numbers: " + j);
     }
     
     //c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     
     for (let k=12;k>=-14;k-=2){
-     //console.log("Even Numbers:" + k);
+     console.log("Even Numbers:" + k);
     }
   
 /*Exercise #2: 
@@ -40,7 +40,7 @@ Construct ``for`` loops to accomplish the following tasks:
  // a. Print each element of the array to a new line.
 
  for (let j= 0;j<arr.length;j++){
-   //console.log("Element of Array : " + arr[j]);
+   console.log("Element of Array : " + arr[j]);
  }
 
  // b. Print each character of the string - in reverse order - to a new line. */ 
@@ -48,13 +48,14 @@ Construct ``for`` loops to accomplish the following tasks:
  for (let i = 0; i < str.length; i++) {
   reverse = str[i] + reverse;
 }
-//console.log("Reversed String: " + reverse); 
+console.log("Reversed String: " + reverse); 
 
 let tempArray = [];
 
 for(let i=0;i<reverse.length;i++ ){
   tempArray[i] = reverse.charAt(i);
-  //console.log("Reversed Character: " +tempArray[i]);
+  
+  console.log("Reversed Character: " +tempArray[i]);
 }
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
@@ -62,12 +63,25 @@ for(let i=0;i<reverse.length;i++ ){
   b. Print the arrays to confirm the results. */
 
   let arr1= [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
-  let even=[];
-  let odd=[];
+  let evenArray=[];
+  let oddArray=[];
+  let a=0;
+  let b=0;
   
-  for ( let i = 0 ; 1 <= arr1.length ; i ++ ) {
-    //if (arr1[i] < arr1.length){
-          even =( arr1[i] % 2 == 0 );
-          console.log(even);
+  for ( let i = 0 ; i < arr1.length ; i ++ ) {
+         
+          //console.log(a);
+          if(arr1[i] % 2 == 0){
+            evenArray[a]=arr1[i];
+            a=a+1;
+          }else{
+            oddArray[b]= arr1[i];
+            b=b+1;
+            
+          }
      } 
+
+     console.log("Even Num Array: " + evenArray);
+     console.log("Odd Num Array: " + oddArray);
+
     
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,5 +1,5 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-let startFuelLevel ='';
+let FuelLevel ='';
 let astronautsAboard= '';
 let altitudeReaches= '';
 
@@ -7,12 +7,21 @@ let altitudeReaches= '';
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+  const input = require('readline-sync');
 
+let fuelLevel = input.question('Please enter a positive number:');
+let int = integer;
+while ((fuelLevel==int )&&(fuelLevel >5000)&&(fuelLevel<30000))
+ {
+   fuelLevel= input.question('Invalid input. Please enter a positive number:');
+   fuelLevel= integer(fuelLevel);
+   console.log(fuelLevel);
+}
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+  ` `
   
   
   
