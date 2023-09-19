59f66282bebee07dba9f3e0b00ff07e06e932fde
anicraj456 <anicraj123@gmail.com>
2023-09-19 16:58:23 -0500

    still working on it

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,3 +1,5 @@
+//const input = require('readline-sync');
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
@@ -23,8 +25,7 @@ cargoHold = [foodCabinet,equipCabinet,petCabinet,sleepCabinet];
 console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-const input = require('readline-sync');
-let userInput = input.question("please select a cabinet (0 to 3): ");
+let userInput = "0";//input.question("please select a cabinet (0 to 3): ");
 userInput=Number(userInput);
 //console.log("User selected cabinet: " + userInput);
 
@@ -38,14 +39,16 @@ if(userInput < cargoHold){
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
 
-const input = require('readline-sync');
-let userCab = input.question("please select a cabinet (0 to 3): ");
+//let userCab = input.question("please select a cabinet (0 to 3): ");
+
+let userCab = 3;
 userCab=Number(userCab);
 console.log(userCab);
-let userItem = input.question("please select an item: ");
-userItem =string(userItem);
+let userItem = "parrots";//input.question("please select an item: ");
+//userItem =string(userItem);
 //userCab.includes(userItem);
-if(userCab.includes(userItem)){
+if(userCab.
+    (userItem)){
     console.log(`selected cabinet item ${userItem} is avaiable in ${userCab}`);
 }else{
     console.log(`selected cabinet item ${userItem} is Not avaiable in ${userCab}`);
