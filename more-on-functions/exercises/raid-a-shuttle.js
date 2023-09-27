92ec538778d7e40d0a618dde84b32bdf1dbf72b6
anicraj456 <anicraj123@gmail.com>
2023-09-26 21:40:56 -0500

    more on function exercise completed

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -24,9 +24,8 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
- 
+// Steal some fuel from the shuttle
+
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
@@ -35,8 +34,22 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Decide where to best place your function call to gather our new fuel.
 
+
+//Function to steal fuel:
+
+let freeRide = function(a) {
+  if (checkFuel(a) === 'green') {
+    return a - 100001;
+  } else if (checkFuel(a) === 'yellow') {
+    return a - 50001;
+  } else {
+    return a;
+  }
+};
+let usedFuel = freeRide(fuelLevel);
+//console.log("used fuel:" + usedFuel);
+
 /* Next, liberate some of that glorious cargo.
- * /
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -44,14 +57,30 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. You need to replace what you steal with something worthless. The count MUST stay the same, or you’ll get caught and thrown into the LaunchCode brig.
 
-//d). Don’t get hasty, matey! Remember to test your function.
+//d). Don’t get hasty, matey! Remember to test your function.*/
+
+//let newArray = [];
+let freeGoods = function(oneArray) {
+  let sArray = cargoHold.splice(3, 2, newArray); /*'salt', 'bronze');*/
+  //console.log(sArray);
+  return sArray;
+};
+//console.log("cargo hold:" + cargoHold);
 
 /* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
- 
+
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
 //b). Call your anonymous fuel and cargo functions from within irs.
 
-//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
+//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."*/
 
+ let irs = function(levelOfFuel, itemsInCargo) {
+  let fuel = freeRide(levelOfFuel);
+  let cargoItems = freeGoods(itemsInCargo);
+  //console.log("cargoHold after stolen: " + cargoHold);
+  return `Raided ${fuel} kg of fuel from the tanks, and stole ${cargoItems[0]} and ${cargoItems[1]} from the cargo hold.`
+};
+
+let newArray = ['oil', 'bronze'];
+console.log(irs(fuelLevel, newArray));
