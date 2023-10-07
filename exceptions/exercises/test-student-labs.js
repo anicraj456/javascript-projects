d97f5c355eecf63efdf11424bce5417965b0e236
anicraj456 <anicraj123@gmail.com>
2023-10-07 18:53:53 -0500

    unit testing completed

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -22,3 +22,27 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+//gradeLabs(studentLabs2);
--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -1,5 +1,6 @@
 function whoWon(player1,player2){
 
+
    if (player1 === player2){
      return 'TIE!';
    }
@@ -12,9 +13,11 @@ function whoWon(player1,player2){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'scissors' && player2 === 'rock '){
+   if (player1 === 'scissors' && player2 === 'rock'){
+    console.log("here");
      return 'Player 2 wins!';
    }
- 
+
    return 'Player 1 wins!';
  }
+ module.exports = whoWon;
