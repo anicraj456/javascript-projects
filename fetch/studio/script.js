ff9ee5446b7b8d0a37f24ecd2dc7a3962de8e0c5
anicraj456 <anicraj123@gmail.com>
2023-11-06 20:27:52 -0600

    working

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -2,19 +2,28 @@
 window.addEventListener("load", function(){
     const contianer = document.getElementById('container');
    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-    console.log(response);
-    response.json().then(function(data){
-    console.log(data);
-
-    for (let i=0;i<data.length;i++){
-        let astronauts = data[i];
+    //console.log(response);
+    response.json().then(function(astFile){
+    console.log(astFile);
+    astFile.sort(function(a,b){
+      return a.hoursInSpace < b.hoursInSpace?1 :-1;
+      //return a.hoursInSpace > b.hoursInSpace?1 :-1; //for least to most
+    });
+    
+    for (let i=0;i<astFile.length;i++){
+        let astronauts = astFile[i];
+        if (astronauts.active === true){
+          activeColor = "green";
+        }else{
+          activeColor = "";
+        }
         contianer.innerHTML += `
         <div class="astronaut">
            <div class="bio">
               <h3>${astronauts.firstName} ${astronauts.lastName}</h3>
                 <ul>
                     <li>Hours in space: ${astronauts.hoursInSpace}</li>
-                    <li>Active: ${astronauts.active}</li>
+                    <li class="activeColor">Active: ${astronauts.active}</li>
                     <li>Skills: ${astronauts.skills.join(", ")}</li>
                 </ul>
             </div>
@@ -24,3 +33,4 @@ window.addEventListener("load", function(){
     });
   });
 });   
+
